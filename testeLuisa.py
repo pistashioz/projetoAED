d47303e63cf6e8f8de1c2c3f52f8e07e2ef9a84d
luisaapinho <40220114@esmad.ipp.pt>
2023-01-22 12:36:36 +0000

    :PPP

--- testeLuisa.py
+++ testeLuisa.py
@@ -20,33 +20,31 @@ def on_select(event):
         '''Faz o botão ser clicável apenas uma vez'''
         btnLike['state'] = DISABLED
     
+    row_id=tree.focus() #Obtem o id da linha ativa
+    linha=tree.item(row_id) #Transforma em uma linha
+
+    image=linha['values'][0]
+    title=linha['values'][1]#Seleciona o nome da linha
+    category=linha['values'][2]
+    description=linha['values'][3]
     
-    
-    #Isso é a variável de número de likes que altera pra um se clicado
-    #ainda é preciso fazer com que esse valor seja armazenado em algum lado pra manter, e fazer com que apenas um usuário por conta pode dar like
+    #Variavel do número de likes que é preciso guardar
     numLikes=IntVar(root) 
+
+    #Name Title que muda de acordo com o jogo que clicamos
     nameTitle=StringVar(root)
+    nameTitle.set(title)
 
-    jogoSelecionado = treeCategoria.item(treeCategoria.selection()[0])["values"]
-    fileJogo = open(ficheiro_jogo, "r")
-    linhas = fileJogo.readlines()
-    for linha in linhas:
-            field=linha.split(";")
-            filename = field[0]
-            jogo = field[1]
-            categoria = field[2]
-            print(jogo)
-            if jogoSelecionado== jogo:
-                nameTitle.set(jogo)
-    
-    print(jogoSelecionado)
-    fileJogo.close()
-    
+    #Categoria q tambem muda de acordo com o jogo q clicamos
+    nameCategory=StringVar(root) 
+    nameCategory.set(category)
+    #descrição que muda de acordo com o jogo que clicamos
     textDescription=StringVar(root)
-    textDescription.set(descricao)
+    textDescription.set(description)
+    
     
 
-    nameCategory=StringVar(root)
+    
    
 
     #Isso sao as medidas da janela que abre quando a função é chamada 
@@ -118,7 +116,7 @@ def contarJogos(numJogos, treeCategoria):
 def consulta_jogo(search_by, numJogos):
 
     
-    treeCategoria.delete(*treeCategoria.get_children())
+    tree.delete(*tree.get_children())
 
     val = search_by.get()
 
@@ -129,12 +127,12 @@ def consulta_jogo(search_by, numJogos):
         filename =  linha.split(";")[0]
         jogo = linha.split(";")[1]
         categoria = linha.split(';')[2]
-        print(f"O filename é {filename}, O jogo é {jogo} e a categoria é {categoria}")
+        descricao=linha.split(';')[3]
         if categoria == val:
-            treeCategoria.insert('', 'end', values=(jogo, categoria))
-        elif val == 'ALL':treeCategoria.insert('', 'end', values = (jogo, categoria))
+            tree.insert('', 'end', values=(jogo, categoria,descricao))
+        elif val == 'ALL':tree.insert('', 'end', values = (jogo, categoria,descricao))
  
-    contarJogos(numJogos, treeCategoria)
+    contarJogos(numJogos, tree)
         
 
 def selecionaJogo():
@@ -216,7 +214,7 @@ window.geometry(f'{appWidth}x{appHeight}+{int(x)}+{int(y)}')
 window.title('myGameList')
 window.resizable(0,0)
 window.iconbitmap('images/video-game-play-pad-boy-gameboy-nintendo_108539.ico')
-window.configure(bg="beige")
+window.configure(bg="black")
 #opcoesBarra
 barraMenu = Menu(window, background='orange', fg='white')
 barraMenu.add_command(label="LIBRARY", command="noaction")
@@ -224,51 +222,76 @@ barraMenu.add_command(label="COMMUNITY", command="noaction")
 barraMenu.add_command(label="ADD A GAME", command=PanelConfigurar)
 window.configure(menu=barraMenu)
 #FrameCatalogo
-frame1 = LabelFrame(window, width=280, height=660, bg='gray35', borderwidth=0, highlightthickness=0)
+frame1 = LabelFrame(window, width=1500, height=660, bg='gray35', borderwidth=0, highlightthickness=0)
 frame1.place(x=0, y=30)
-frameNewGames = LabelFrame(window, width = 920, height=220, relief = "ridge", bg='RoyalBlue4', borderwidth=0, highlightthickness=0)
-frameNewGames.place(x=280, y=30)
-frame2 = LabelFrame(window, width = 920, height=450, bg='gray45', borderwidth=0, highlightthickness=0)
-frame2.place(x=280, y=250)
-frame4 = LabelFrame(window,width=280, height=100, bg='gray13', borderwidth=0, highlightthickness=0)
-frame4.place(x=0, y=30)
-frameLoginBackground = LabelFrame(window,width=1200,height=30, bg="thistle",borderwidth=0, highlightthickness=0)
-frameLoginBackground.place(x=0, y=0)
-frameLinha1 = LabelFrame(frameNewGames,width=850,height=1, bg="white",borderwidth=0, highlightthickness=0)
-frameLinha1.place(x=130, y=20)
-frame5 = LabelFrame(window,width=280, height=50, bg='blue', borderwidth=0, highlightthickness=0)
+
+
+
+frame5 = LabelFrame(window,width=165, height=50, bg='orange1', borderwidth=0, highlightthickness=0)
 frame5.place(x=0, y=530)
-# Treeview New Games
-
-
-
-
-# Add the rowheight and vertical scrollbar
 
 s=ttk.Style()
 s.configure('Treeview', rowheight=150)
 
 global tree
 
-tree = ttk.Treeview(frameNewGames, selectmode='browse', columns = ('Image', 'Name', 'Category'))
-treeScroll = Scrollbar(tree, orient = 'vertical')
-treeScroll.pack(side = 'right', fill = 'y')
-tree.place(x=0, y=35)
+tree = ttk.Treeview(frame1, selectmode='browse', columns = ('Image', 'Name', 'Category','Description'))
+tree.bind('<<TreeviewSelect>>', on_select)
+tree.place(x=165, y=40)
+style = ttk.Style(window)
 
-tree = ttk.Treeview(frameNewGames, selectmode='browse', columns = ('Image', 'Name', 'Category'), yscrollcommand=treeScroll.set)
-tree.place(x=0, y=35)
 
-tree.column('Image', width =100, anchor = 'center', stretch=False)
-tree.column('Name', width = 350, anchor = 'center', stretch=False)
-tree.column('Category', width = 350, anchor = 'center', stretch=False)
+style.configure("Treeview", background="gray13", 
+                fieldbackground="black", foreground="white")
 
+
+tree.column('Image', width =200, anchor = 'center', stretch=False)
+tree.column('Name', width = 200, anchor = 'center', stretch=False)
+tree.column('Category', width = 200, anchor = 'center', stretch=False)
+tree.column('Description', width = 200, anchor = 'center', stretch=False)
 
 tree.heading('Image', text = 'Image')
 tree.heading('Name', text = 'Name')
 tree.heading('Category', text = 'Category')
+tree.heading('Description', text = 'Description')
 
 
 
+
+
+#TOTAL GAMES
+lbNumJogos = Label(frame5, text = "Nº Games:", font = ("Helvetica", "12"), bg = 'orange1')
+lbNumJogos.place(x=0, y=10)
+
+numJogos = StringVar()
+txt_num_jogos = Entry(frame5, width=10, textvariable = numJogos)
+txt_num_jogos.place(x=80, y=15)
+
+
+
+# Search Category
+
+column = []
+fileCategoria = open('files/categorias.txt', 'r')
+for line in fileCategoria:
+    column.append(line.strip())
+fileCategoria.close()
+search_by = ttk.Combobox(frame1, values = column, width = 164, height= 400)
+search_by.current(0)
+search_by.place(x = 160, y = 0)
+# Search Text
+
+# Search Button
+btnSearch = Button(frame1, text='Search', width=21, height=2, bg="gray13", fg="white", command= lambda: (consulta_jogo(search_by, numJogos)))
+btnSearch.place(x=0, y = 0)
+
+#  Scrollbar Vertical
+verscrlbar = ttk.Scrollbar(window, orient ="vertical", command = tree.yview)
+# CallinPlace da Scrollbar
+verscrlbar.place(x=1155, y=60, height=300)
+# Adicionar scrollbar à  treeview
+tree.configure(yscrollcommand = verscrlbar.set)
+#treeview Jogos por categorias
 
 jogos = ler_jogo()
 
@@ -279,82 +302,25 @@ for dato in jogos:
     categoria = dato[2]
     descricao=dato[3]
     imgs.append(tk.PhotoImage(file=filename))
-    tree.insert('', 'end', image = imgs[-1], value = (filename, jogo, categoria,descricao))
-
-
-
-
-#  Scrollbar Vertical
-verscrlbar = ttk.Scrollbar(frameNewGames, orient ="vertical", command = tree.yview)
-# CallinPlace da Scrollbar
-verscrlbar.place(x=905, y=20, height=250)
-# Adicionar scrollbar à  treeview
-tree.configure(yscrollcommand = verscrlbar.set)
-#treeview Jogos por categorias
-
-global treeCategoria
-
-treeCategoria = ttk.Treeview(frame1, selectmode= 'browse', columns = ('jogos', 'categoria'), show = 'headings')
-treeCategoria.bind('<<TreeviewSelect>>', on_select)
-
-treeCategoria.column('jogos', width = 120, anchor = 'center')
-treeCategoria.heading('jogos', text = 'Jogos')
-treeCategoria.column('categoria', width = 120, anchor = 'center')
-treeCategoria.heading('categoria', text = 'Categoria')
-treeCategoria.place(x = 20, y=110)
-
-#TOTAL GAMES
-lbNumJogos = Label(frame5, text = "Nº Games:", font = ("Helvetica", "15"), bg = 'blue')
-lbNumJogos.place(x=40, y=10)
-
-numJogos = StringVar()
-txt_num_jogos = Entry(frame5, width=15, textvariable = numJogos)
-txt_num_jogos.place(x=150, y=15)
-
-
-lblWhatsNew = Label(frameNewGames, text = 'NEW GAMES', font=('Helvetica', 12, "bold"), bg="RoyalBlue4", fg="white")
-lblWhatsNew.place(x=20, y=10)
-lblMostViewed = Label(frame2, text = 'MOST VIEWED', font=('Helvetica', 12, "bold"), bg="gray45", fg="white")
-lblMostViewed.place(x=20, y=20)
-lblMostLiked = Label(frame2, text='MOST LIKED', font=('Helvetica', 12, "bold"), bg="gray45", fg="white")
-lblMostLiked.place(x=20, y=150)
-lblMyFavorites = Label(frame2, text = 'MY FAVORITES', font=('Helvetica', 12, "bold"), bg="gray45", fg="white")
-lblMyFavorites.place(x=20, y=280)
-
-# Search Category
-
-column = []
-fileCategoria = open('files/categorias.txt', 'r')
-for line in fileCategoria:
-    column.append(line.strip())
-fileCategoria.close()
-search_by = ttk.Combobox(frame4, values = column, width = 43, height= 100)
-search_by.current(0)
-search_by.place(x = 0, y = 0)
-# Search Text
-
-# Search Button
-btnSearch = Button(frame4, text='Search', width=25, height=2, bg="gray13", fg="white", command= lambda: (consulta_jogo(search_by, numJogos)))
-btnSearch.place(x= 50, y = 40)
-
+    tree.insert('', 'end', image = imgs[-1], value = ('', jogo, categoria, descricao))
 
 
 # Login Button
 btnLogin = Button(window, text="Login",font=('Helvetica', 10), width=6, height=1, bg="orange", fg="black", command= lambda: logInInterface(windowFechar))
-btnLogin.place(x = 985, y = 1)
+btnLogin.place(x = 1000, y = 1)
 # Create Account Button
 btnCreateAccount = Button(window, text="Create Account",font=('Helvetica', 10), width=12, height=1, bg="orange", fg="black", command=signUp)
-btnCreateAccount.place(x = 1095, y = 1)
+btnCreateAccount.place(x = 1065, y = 1)
 
-btnCreateCategory = Button(window, text="Create Category",font=('Helvetica', 10), width=12, height=1, bg="blue", fg="black", command= lambda: adicionarCategoria(newCategoria, search_by))
-btnCreateCategory.place(x = 10, y = 1)
+btnCreateCategory = Button(frame1, text="Create Category",font=('Helvetica', 10), width=12, height=1, bg="orange1", fg="black", command= lambda: adicionarCategoria(newCategoria, search_by))
+btnCreateCategory.place(x =24, y =170)
 
 newCategoria = StringVar()
-txtCategoriaAdd = Entry(window, textvariable=newCategoria, font=('Helvetica', 15), width=15)
-txtCategoriaAdd.place(x=120, y = 1)
+txtCategoriaAdd = Entry(frame1, textvariable=newCategoria, font=('Helvetica', 15), width=10)
+txtCategoriaAdd.place(x=20, y =100)
 
-btnDeleteCategory = Button(window, text="Delete Category",font=('Helvetica', 10), width=20, height=1, bg="red", fg="black", command= lambda: removerCategoria(search_by))
-btnDeleteCategory.place(x = 300, y = 1)
+btnDeleteCategory = Button(frame1, text="Delete Category",font=('Helvetica', 10), width=12, height=1, bg="orange3", fg="black", command= lambda: removerCategoria(search_by))
+btnDeleteCategory.place(x =24, y =210)
 
 
 
